e04acb0e33d9061cf8d145152a547a887b58584d
depst0r <depstor90@gmail.com>
2023-05-06 13:07:14 +0300

    addZero

--- js/script.js
+++ js/script.js
@@ -6,9 +6,17 @@ window.addEventListener("DOMContentLoaded", function () {
   const updateClock = () => {
     const time = new Date();
     const hours = time.getHours();
-    const minutes = time.getMinutes();
+    const minutes = addZero(time.getMinutes());
     clock.textContent = `${hours}:${minutes}`;
   };
+
+  function addZero(num) {
+    if (num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
 
   const clockStart = () => {
     setInterval(updateClock, 1000);
@@ -16,4 +24,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
   clockStart();
   updateClock();
+
+  //burger menu
+
+  // const menu = document.querySelector(".fa-bars");
+  // const cross = document.querySelector(".fa-times");
 });
